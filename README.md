d977d6738599cdcd76d70e58907cf90a0492e7f5
phsequeira <phsequeira@yahoo.com>
2021-01-25 13:58:26 -0800

    add plan

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# EDIT-THIS-README
+# poke catcher
+
+## HTML
+    -instructions
+    -hidden div
+        -three different clickable pokemon img
+    
+    results page
+    -display catches
+    -display encounters
+
+## plan
+    -load three pokemon
+        -generate 3 random pokemon
+        -make sure none of the 3 match
+    -on click
+        -increment pokemon seen
+        -increment caught pokemon
+        -three new pokemon show up on page
+        -if user does this 10x redirect to results page
+        -else repeat
+
+
+    
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,26 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { getRandomPokemon } from '../utils.js';
+
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('get three random pokemon that do not match', (expect) => {
     //Arrange
+    let pokeOne = getRandomPokemon();
+    let pokeTwo = getRandomPokemon();
+    let pokeThree = getRandomPokemon();
+
+    while (pokeOne.id === pokeTwo.id || pokeOne.id === pokeThree.id || pokeTwo.id === pokeThree.id) {
+        pokeOne = getRandomPokemon();
+        pokeTwo = getRandomPokemon();
+        pokeThree = getRandomPokemon();
+    }
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = pokeOne.id !== pokeTwo.id || pokeOne.id !== pokeThree.id || pokeTwo.id !== pokeThree.id;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = pokeOne.id, pokeTwo.id, pokeThree.id;
 
     //Expect
     // Make assertions about what is expected versus the actual result
