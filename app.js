bcac0a0aa3e87d5792701486caec82fae9c7465e
phsequeira <phsequeira@yahoo.com>
2021-01-25 14:23:21 -0800

    three random pokemon img shows up when reload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // import functions and grab DOM elements
 import pokemon from './data.js';
-
+import { setThreePokemon } from './utils.js';
 // initialize state
 
 // set event listeners to update state and DOM
 
+setThreePokemon();
--- utils.js
+++ utils.js
@@ -10,3 +10,27 @@ function findById(array, id) {
         if (item.pokemon === id) return item;
     }
 }
+function renderPokeImage(pokemonItem) {
+    const image = document.createElement('img');
+    image.className = 'poke-img';
+    image.src = pokemonItem.url_image;
+    return image;
+}
+export function setThreePokemon() {
+    let pokeOne = getRandomPokemon();
+    let pokeTwo = getRandomPokemon();
+    let pokeThree = getRandomPokemon();
+
+    while (pokeOne.id === pokeTwo.id || pokeOne.id === pokeThree.id || pokeTwo.id === pokeThree.id) {
+        pokeOne = getRandomPokemon();
+        pokeTwo = getRandomPokemon();
+        pokeThree = getRandomPokemon();
+    }
+
+    const img1 = renderPokeImage(pokeOne);
+    const img2 = renderPokeImage(pokeTwo);
+    const img3 = renderPokeImage(pokeThree);
+
+    const div = document.getElementById('poke-div');
+    div.append(img1, img2, img3);
+}
